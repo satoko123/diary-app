90636bb3f3ec31a97f37f3a08839979e3b3462ba
satoko123 <moonysweets@gmail.com>
2021-05-05 23:52:28 +0900

    アソシエーション設定

--- app/models/feed_management.rb
+++ app/models/feed_management.rb
@@ -1,2 +1,6 @@
 class FeedManagement < ApplicationRecord
+  before_save do
+    self.amount_eaten = yesterday_leftover + morning_addition + noon_addition + evening_addition - today_leftover
+  end
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ApplicationRecord
          
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
+  
+  has_many :feed_managements
 end
