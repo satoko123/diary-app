d7af3cd5422dd8392ca4574ba51920171baa7e07
satoko123 <moonysweets@gmail.com>
2021-05-05 23:52:11 +0900

    ルーティングの設定

--- app/controllers/feed_managements_controller.rb
+++ app/controllers/feed_managements_controller.rb
@@ -1,2 +1,26 @@
 class FeedManagementsController < ApplicationController
+  def new
+    @feed_management = FeedManagement.new
+
+    if FeedManagement.exists?
+      @feed_management_last = FeedManagement.last 
+    end
+  end
+
+  def create
+    @feed_management = FeedManagement.new(feed_management_params)
+    if @feed_management.save
+      redirect_to root_path, flash: {success: "ご飯記録を保存しました"}
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def feed_management_params
+    params.require(:feed_management).permit(:yesterday_leftover, :morning_addition, :noon_addition, :evening_addition, :today_leftover,
+                                 :amount_eaten, :created_on).merge(user_id: current_user.id)
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   get 'diaries/index'
   devise_for :users
   root to: "diaries#index"
-  resources :feed_managements, only: :new
+  resources :feed_managements, only: [:new, :create]
 end
