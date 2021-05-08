0bf55933b4a7822415f9d9d85fe3e4ad8899763b
satoko123 <moonysweets@gmail.com>
2021-05-08 23:58:07 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'diaries/index'
   devise_for :users
   root to: "diaries#index"
   resources :feed_managements, only: [:new, :create, :edit, :update]
+  resources :diaries
 end
