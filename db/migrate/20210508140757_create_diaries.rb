68991109ab2e7d3ccba60f3a789bc28b37356605
satoko123 <moonysweets@gmail.com>
2021-05-08 23:22:42 +0900

    テーブル作成

---
+++ db/migrate/20210508140757_create_diaries.rb
@@ -0,0 +1,13 @@
+class CreateDiaries < ActiveRecord::Migration[6.0]
+  def change
+    create_table :diaries do |t|
+      t.string :text
+      t.float :weight
+      t.date :created_on, null: false, unique: true
+      t.references :user, null: false, foreign_key: true
+      t.references :feed_management, null: false, foreign_key: true
+      t.timestamps
+    end
+    add_index :diaries, [:created_on], unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_04_145701) do
+ActiveRecord::Schema.define(version: 2021_05_08_140757) do
+
+  create_table "diaries", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "text"
+    t.float "weight"
+    t.date "created_on", null: false
+    t.bigint "user_id", null: false
+    t.bigint "feed_management_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["created_on"], name: "index_diaries_on_created_on", unique: true
+    t.index ["feed_management_id"], name: "index_diaries_on_feed_management_id"
+    t.index ["user_id"], name: "index_diaries_on_user_id"
+  end
 
   create_table "feed_managements", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "yesterday_leftover", default: 0, null: false
@@ -39,5 +52,7 @@ ActiveRecord::Schema.define(version: 2021_05_04_145701) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "diaries", "feed_managements"
+  add_foreign_key "diaries", "users"
   add_foreign_key "feed_managements", "users"
 end
