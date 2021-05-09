a6dce44c9205922ce03aae55423f015593b7fe98
satoko123 <moonysweets@gmail.com>
2021-05-10 02:37:07 +0900

    テストコードの実装

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -1,9 +1,11 @@
 class Diary < ApplicationRecord
   # バリデーション
+  validates :weight, numericality: { allow_nil: true, message: 'は半角数字で入力してください' }
   validates :created_on, uniqueness: true
 
+  
   # アソシエーション
   belongs_to :user
   belongs_to :feed_management
-  has_one_attached :images # active_storageのためのアソシエーション
+  has_many_attached :images # active_storageのためのアソシエーション
 end
--- spec/factories/diaries.rb
+++ spec/factories/diaries.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :diary do
-    
+      text       {"メモ"}
+      weight     {2.5}
+      created_on {Faker::Date.between(from: '2014-09-23', to: '2021-04-1')}
+      association :user 
+      association :feed_management
+
+      after(:build) do |diary|
+        diary.images.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+      end
   end
 end
