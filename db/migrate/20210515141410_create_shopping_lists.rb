7fb5910f01475af141f68358859855581caa75a8
satoko123 <moonysweets@gmail.com>
2021-05-15 23:19:38 +0900

    テーブルの作成

--- db/migrate/20210515141410_create_shopping_lists.rb
+++ db/migrate/20210515141410_create_shopping_lists.rb
@@ -1,8 +1,9 @@
 class CreateShoppingLists < ActiveRecord::Migration[6.0]
   def change
     create_table :shopping_lists do |t|
-
+      t.string    :item_name, null: false
       t.timestamps
     end
+    add_index :shopping_lists, [:item_name], unique: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_08_152138) do
+ActiveRecord::Schema.define(version: 2021_05_15_141410) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -59,6 +59,13 @@ ActiveRecord::Schema.define(version: 2021_05_08_152138) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["created_on"], name: "index_feed_managements_on_created_on", unique: true
     t.index ["user_id"], name: "index_feed_managements_on_user_id"
+  end
+
+  create_table "shopping_lists", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "item_name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["item_name"], name: "index_shopping_lists_on_item_name", unique: true
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
