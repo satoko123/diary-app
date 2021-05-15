8f94d8ca61595ef1081ffb468d748f5c2312f9b4
satoko123 <moonysweets@gmail.com>
2021-05-15 23:47:27 +0900

    Javascriptの実装の準備

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -1,4 +1,10 @@
 class ShoppingListsController < ApplicationController
   def index
+    @shopping_lists = ShoppingList.all.order(id: "DESC")
   end
 end
+
+  def create
+    ShoppingList.create(item_name: params[:item_name])
+    redirect_to action: :index
+  end
