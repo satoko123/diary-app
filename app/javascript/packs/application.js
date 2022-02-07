674b9d53b3ef8e0c8b11b165960bc76b5ec40bb3
satoko123 <moonysweets@gmail.com>
2022-02-07 11:52:42 +0900

    chartkickの導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,6 +13,8 @@ require("../add_item")
 require("../check")
 require("../order")
 require("../image_destroy")
+require("chartkick")
+require("chart.js")
 // require("../preview_destroy")
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
